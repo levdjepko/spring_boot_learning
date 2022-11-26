ded51853980d789e649e8e8dba6f3ec978c9015c
Lev <levdjepko@gmail.com>
2022-11-25 20:29:21 -0800

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -5,13 +5,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/")
     public String index() {
         return "Hello Spring Boot!";
     }
-    
-    
+        
  
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
