8d3002b0ea0d965f3e32629d208d54355b7b9eea
Lev <levdjepko@gmail.com>
2023-03-18 22:34:07 -0700

    Update HelloController.java

--- HelloController.java
+++ HelloController.java
@@ -1,5 +1,4 @@
 package com.example.demo;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,6 @@ public class HelloController {
         return "Hello Spring Boot!";
     }
         
- 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.OK)
   void deleteSOMETHING(@PathVariable Long id) {
